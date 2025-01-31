c4bdd7d8ce7647c71402949680b865cb1d03ddfb
Sinitsa <sinitsammi@gmail.com>
2025-01-31 09:59:17 +0300

    Program

--- Сollections/Program.cs
+++ Сollections/Program.cs
@@ -8,50 +8,36 @@ namespace Сollections
     {
         static void Main(string[] args)
         {
-            Dictionary<string, string> dictionary = new Dictionary<string, string>
-            {
-                {"авария","Повреждение, выход из строя какого-н. механизма, машины, устройства во время работы, движения."},
-                {"август","Восьмой месяц календарного года."},
-                {"автобиография","Описание своей жизни."},
-                {"бактерия","Микроорганизм, преимущ. одноклеточный."},
-                {"баланс","Сравнительный итог прихода и расхода"},
-                {"казан","Котёл для приготовления пищи."},
-                {"кабинет","Комната для занятий, работы."},
-                {"казнь","Лишение жизни как высшая карающая мера."}
-            };
+            int shopBalanse = 0;
+            int shopBalansePosX = 50;
+            int shopBalansePosY = 0;
 
-            Console.InputEncoding = Encoding.Unicode;
+            Queue<int> clients = new Queue<int>(new int[] { 1, 43, 5, 1, 8, 1, 9 });
+
             Console.OutputEncoding = Encoding.Unicode;
 
-            bool isActive = true;
-            string exitCommand = "exit";
-            string errorMessage = "Неверный ввод или такого слова нет в словаре.";
-
-            while (isActive)
+            while (clients.Count > 0)
             {
-                Console.WriteLine($"\nВведите слово и узнайте его значение.\nВведите {exitCommand} для выходы.");
-                string playerInput = Console.ReadLine();
+                Console.WriteLine("Клиент пришел");
+                MakePurchase(ref shopBalanse, clients.Dequeue());
+                Console.WriteLine("Клиент ушел");
+                ShowBalanse(shopBalansePosX, shopBalansePosY, shopBalanse);
+
+                Console.ReadKey();
                 Console.Clear();
-
-                if (playerInput == exitCommand)
-                {
-                    isActive = false;
-                }
-                else
-                {
-                    Console.WriteLine(playerInput.ToUpper());
-                    Console.WriteLine(GetDescription(dictionary, playerInput, errorMessage));
-                }
             }
-
         }
 
-        private static string GetDescription(Dictionary<string, string> dictionary, string word, string errorMessage)
+        private static void MakePurchase(ref int shopBalanse,int cost)
         {
-            if (dictionary.ContainsKey(word.ToLower()))
-                return dictionary[word.ToLower()]; 
-            else            
-                return errorMessage;                     
+            Console.WriteLine($"Клиент потратил {cost} $");
+            shopBalanse += cost;
+        }
+
+        private static void ShowBalanse(int posX, int posY, int balanse)
+        {
+            Console.SetCursorPosition(posX, posY);
+            Console.Write($"БАЛАНС магазина: {balanse}");
         }
     }
 }
