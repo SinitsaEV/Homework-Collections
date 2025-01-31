542ed8101b56fa8e9823cfc8f2eaf4b9e682cc26
Sinitsa <sinitsammi@gmail.com>
2025-01-31 11:28:29 +0300

    Program

--- Сollections/Program.cs
+++ Сollections/Program.cs
@@ -8,36 +8,81 @@ namespace Сollections
     {
         static void Main(string[] args)
         {
-            int shopBalanse = 0;
-            int shopBalansePosX = 50;
-            int shopBalansePosY = 0;
+            const string ExitCommand = "exit";
+            const string SumCommand = "sum";
 
-            Queue<int> clients = new Queue<int>(new int[] { 1, 43, 5, 1, 8, 1, 9 });
+            List<int> numbers = new List<int>();
+
+            bool isRunning = true;
 
             Console.OutputEncoding = Encoding.Unicode;
 
-            while (clients.Count > 0)
-            {
-                Console.WriteLine("Клиент пришел");
-                MakePurchase(ref shopBalanse, clients.Dequeue());
-                Console.WriteLine("Клиент ушел");
-                ShowBalanse(shopBalansePosX, shopBalansePosY, shopBalanse);
+            ShowMenu(ExitCommand, SumCommand);
 
-                Console.ReadKey();
-                Console.Clear();
+            while (isRunning)
+            {
+                string playerInput = Console.ReadLine();
+
+                switch (playerInput)
+                {
+                    case ExitCommand:
+                        isRunning = false;
+                        Console.WriteLine("Вы вышли из программы.");
+                        break;
+
+                    case SumCommand:
+                        GetSum(numbers);
+                        break;
+
+                    default:
+                        TryAddNumber(playerInput, numbers);
+                        break;
+                }
             }
         }
 
-        private static void MakePurchase(ref int shopBalanse,int cost)
+        private static int GetSum(List<int> numbers)
         {
-            Console.WriteLine($"Клиент потратил {cost} $");
-            shopBalanse += cost;
+            int sum = 0;
+
+            foreach (int number in numbers)
+                sum += number;
+
+            Console.WriteLine("Сумма всех чисел: " + sum);
+            return sum;
         }
 
-        private static void ShowBalanse(int posX, int posY, int balanse)
+        private static void ShowMenu(string exitCommand, string sumCommand)
         {
-            Console.SetCursorPosition(posX, posY);
-            Console.Write($"БАЛАНС магазина: {balanse}");
+            Console.WriteLine("Команды программы:");
+            Console.WriteLine($"Выйти из программы - {exitCommand}\nВывести сумму всех чисел - {sumCommand}\nВведите любое число для добавления.");
+        }
+
+        private static void TryAddNumber(string playerInput, List<int> numbers)
+        {
+            if (int.TryParse(playerInput, out int playerNumber))
+            {
+                numbers.Add(playerNumber);
+                Console.WriteLine($"Добавлено число: {playerInput}");
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод");
+            }
         }
     }
 }
+
+//В массивах вы выполняли задание "Динамический массив"
+
+//Используя всё изученное, напишите улучшенную версию динамического массива(не обязательно брать своё старое решение)
+
+//Задание нужно, чтобы вы освоились с List и прощупали его преимущество. 
+
+//Проверка на ввод числа обязательна.
+
+//Пользователь вводит числа, и программа их запоминает. 
+
+//Как только пользователь введёт команду sum, программа выведет сумму всех введенных чисел. 
+
+//Выход из программы должен происходить только в том случае, если пользователь введет команду exit.
