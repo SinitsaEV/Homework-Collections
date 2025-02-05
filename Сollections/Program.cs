e32686398b5945b77d2a70999c4576d84b5d7cf7
Sinitsa <sinitsammi@gmail.com>
2025-02-05 15:14:58 +0300

    Program

--- Сollections/Program.cs
+++ Сollections/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Сollections
 {
@@ -8,121 +7,42 @@ namespace Сollections
     {
         static void Main(string[] args)
         {
-            const string AddCommand = "0";
-            const string RemoveCommand = "1";
-            const string ShowCommand = "2";
-            const string ExitCommand = "3";
+            string[] array1 = { "1", "2", "3", "1" };
+            string[] array2 = { "1", "4", "5" };
 
-            Dictionary<string, List<string>> personnelAccounting = new Dictionary<string, List<string>>
+            List<string> result = MergeUniqueElements(array1, array2);
+            ShowListElements(result);
+        }
+
+        private static List<string> MergeUniqueElements(string[] array1, string[] array2)
+        {
+            List<string> uniqueElements = new List<string>();
+
+            foreach (string element in array1)
             {
-                {"курьер",new List<string>{"Наркевич"} },
-                {"слесарь",new List<string>{"Илюха","Андрей" } },
-                {"химик",new List<string>{ "Яна"} },
-
-            };
-            
-            Console.OutputEncoding = Encoding.Unicode;
-            Console.InputEncoding = Encoding.Unicode;
-
-            bool isActive = true;
-
-            while (isActive)
-            {
-                Console.WriteLine($"{AddCommand} - добавить\n{RemoveCommand} - удалить\n{ShowCommand} - показать всех\n{ExitCommand} - выход");
-
-                string post;
-                string fullName;
-                string playerInput = Console.ReadLine();
-
-                switch (playerInput)
+                if(!uniqueElements.Contains(element))
                 {
-                    case AddCommand:
-                        GetEmployeeData(out post, out fullName);
-                        AddEmployee(personnelAccounting, post, fullName);
-                        break;
-
-                    case RemoveCommand:
-                        GetEmployeeData(out post, out fullName);
-                        RemoveEmployee(personnelAccounting, post, fullName);
-                        RemoveEmptyPosts(personnelAccounting);
-                        break;
-
-                    case ShowCommand:
-                        ShowStaffInformation(personnelAccounting);
-                        break;
-
-                    case ExitCommand:
-                        Console.WriteLine("Вы вышли из программы.");
-                        isActive = false;
-                        break;
-
-                    default:
-                        Console.WriteLine("Неверный ввод.");
-                        break;
+                    uniqueElements.Add(element);
                 }
             }
-        }
 
-        private static void AddEmployee(Dictionary<string, List<string>> staff, string post, string fullName)
-        {
-            if (staff.TryGetValue(post, out List<string> staf))
-                staf.Add(fullName);
-            else
-                staff.Add(post, new List<string> { fullName });
-        }
-
-        private static void RemoveEmployee(Dictionary<string, List<string>> staff, string post, string fullName)
-        {
-            if (staff.ContainsKey(post))
-                if (staff[post].Contains(fullName))
-                    staff[post].Remove(fullName);
-        }
-
-        private static void RemoveEmptyPosts(Dictionary<string, List<string>> staff)
-        {
-            List<string> removePosts = GetEmptyPosts(staff);
-
-            RemovePosts(removePosts,staff);            
-        }
-
-        private static List<string> GetEmptyPosts(Dictionary<string, List<string>> staff)
-        {
-            List<string> removePosts = new List<string>();
-
-            foreach (string post in staff.Keys)
+            foreach (string element in array2)
             {
-                if (staff[post].Count > 0)
-                    continue;
-
-                removePosts.Add(post);
+                if (!uniqueElements.Contains(element))
+                {
+                    uniqueElements.Add(element);
+                }
             }
 
-            return removePosts;
+            return uniqueElements;
         }
 
-        private static void ShowStaffInformation(Dictionary<string, List<string>> staff)
+        private static void ShowListElements(List<string> list)
         {
-            foreach (string post in staff.Keys)
+            foreach (string s in list)
             {
-                Console.WriteLine($"Должность : {post}");
-
-                foreach(string person in staff[post])
-                    Console.WriteLine($" - {person}");
+                Console.Write(s + " ");
             }
-        }
-
-        private static void RemovePosts(List<string> removePosts, Dictionary<string, List<string>> staff)
-        {
-            foreach(string post in removePosts)
-                staff.Remove(post);
-        }
-
-        private static void GetEmployeeData(out string post,out string fullName)
-        {
-            Console.Write("Введите должность работника:");
-            post = Console.ReadLine().ToLower();
-            Console.Write("Введите ФИО работника:");
-            fullName = Console.ReadLine();
-        }
+        }        
     }
 }
